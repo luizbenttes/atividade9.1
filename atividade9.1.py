4da481931beb15723e1a127af57d134ea56c8085
luizbenttes <luizfbenttes@gmail.com>
2017-11-19 14:02:46 -0400

    Atualização da Interface, Solução de Bugs

--- atividade9.1.py
+++ atividade9.1.py
@@ -3,7 +3,8 @@ from tkinter import Menu
 from tkinter import ttk
 from tkinter.messagebox import showinfo
 import pandas as pd
-
+from PIL import ImageTk
+import gc
 
 APP_TITLE = "SIMTON"
 APP_XPOS = 300
@@ -15,14 +16,16 @@ IMAGE_PATH = "images/"
 
 
 class Aresta(object):
-	def __init__(self, canvas, comprimento, lista, No1, No2):
+	def __init__(self, canvas, comprimento, lista, No1, No2, valorLambda):
 		self.comprimento = comprimento
 		self.No1 = No1
 		self.No2 = No2
+		self.valorLambda = valorLambda 
 		self.canvas = canvas
 		self.lista = lista
 		self.message = tk.StringVar()
 		self.message.set(self.comprimento)
+		self.on = True
 
 		self.posicaoAresta = self.lista[self.No1].posicaoAtual + self.lista[self.No2].posicaoAtual
 		self.aresta = self.canvas.create_line(self.posicaoAresta, width=3)
@@ -35,37 +38,45 @@ class Aresta(object):
 		y = (self.lista[No1].posicaoAtual[1] + self.lista[No2].posicaoAtual[1]) / 2
 		canvas.create_window(x, y, window=self.widget)
 
-
 	def deleta_Aresta(self):
+		self.on = False
 		self.canvas.delete(self.aresta)
 		self.widget.destroy()
 		self.win.destroy()
 
 	def configura_Aresta(self):
 		self.win = tk.Toplevel()
-
-		self.win.wm_title(str(self.lista[self.No1].nome_No+"->"+self.lista[self.No2].nome_No))
-		self.win.geometry("%dx%d%+d%+d" % (350, 100, 100, 200))
-
-		r = tk.Label(self.win, text=" ")
-		r.grid(row=1, column=0)
+		self.win.wm_title("Aresta: "+str(self.lista[self.No1].nome_No+"->"+self.lista[self.No2].nome_No))
+		self.win.iconbitmap(r'images\favicon.ico')
+		self.win.geometry("%dx%d%+d%+d" % (280, 150, 100, 200))
 
 		l = tk.Label(self.win, text="Distância: ")
-		l.grid(row=1, column=1)
+		l.place(relx=0.25, rely=0.2, anchor="c")
 
 
 		self.distancia = tk.Entry(self.win)
 		self.distancia.insert(10, self.comprimento)
-		self.distancia.grid(row=1, column=2)
+		self.distancia.place(relx=0.7, rely=0.2, anchor="c")
 
-		v = tk.Label(self.win, text=" ")
-		v.grid(row=2, column=3)
 
-		t = ttk.Button(self.win, text="Salvar", command=self.salva_conf)
-		t.grid(row=1, column=3)
+		l2 = tk.Label(self.win, text="Lambda: ")
+		l2.place(relx=0.25, rely=0.4, anchor="c")
 
-		self.k = ttk.Button(self.win, text="Exclui Aresta", command=self.deleta_Aresta)
-		self.k.grid(row=3, column=3)
+		self.valorLamb = tk.Entry(self.win)
+		self.valorLamb.insert(10, self.valorLambda)
+		self.valorLamb.place(relx=0.7, rely=0.4, anchor="c")
+
+		t = tk.Button(self.win, borderwidth=0, highlightthickness=0, command=self.salva_conf)
+		self.img_salvar = ImageTk.PhotoImage(file="images/button_salvar.png")
+		t.config(image=self.img_salvar)
+		self.image = self.img_salvar
+		t.place(relx=0.3, rely=0.90, anchor="s")
+
+		self.k = tk.Button(self.win, borderwidth=0, highlightthickness=0, command=self.deleta_Aresta)
+		self.img_apagarAresta = ImageTk.PhotoImage(file="images/button_apagar-aresta.png")
+		self.k.config(image=self.img_apagarAresta)
+		self.image = self.img_apagarAresta
+		self.k.place(relx=0.7, rely=0.90, anchor="s")
 
 	def salva_conf(self):
 		self.comprimento = self.distancia.get()
@@ -75,11 +86,11 @@ class Aresta(object):
 
 class No(object):
 	def __init__(self, canvas, image_name, xpos, ypos, cont):
-
 		self.posicaoAtual = [xpos,ypos]
 		self.arestasAdj = []
 		self.nosAdj = []
 		self.qtdMoves = 0
+		self.on = True
 
 		self.nome_No = str(cont) #'Nó '+
 		self.canvas = canvas
@@ -92,38 +103,47 @@ class No(object):
 		self.canvas.itemconfig(self.canvas_id, text = self.nome_No, font='Helvetica 10')
 
 		canvas.tag_bind(self.image_obj, '<Double-Button-1>', self.set_No)
-		canvas.tag_bind(self.image_obj, '<Button1-Motion>',self.move)
+		canvas.tag_bind(self.image_obj, '<Button1-Motion>',	 self.move)
 		canvas.tag_bind(self.image_obj, '<ButtonRelease-1>', self.release)
-
 
 		self.move_flag = False
 
+	def __del__(self):
+		print("deletado")
+
 	def set_No(self,event):
 		self.win = tk.Toplevel()
-		self.win.wm_title(self.nome_No)
-		self.win.geometry("%dx%d%+d%+d" % (270,70, self.xpos, self.ypos))
+		self.win.wm_title("Configurações: "+self.nome_No)
+		self.win.iconbitmap(r'images\favicon.ico')
+		self.win.geometry("%dx%d%+d%+d" % (290,85, self.xpos, self.ypos))
+
 		l = tk.Label(self.win, text="Nome do Nó: ")
-		l.grid(row=1, column=1)
+		l.place(relx=0.23, rely=0.2, anchor="n")
 
 		self.nome_NoAt = tk.Entry(self.win)
 		self.nome_NoAt.insert(10, self.nome_No)
-		self.nome_NoAt.grid(row=1, column=2)
+		self.nome_NoAt.place(relx=0.65, rely=0.2, anchor="n")
 
-		space = tk.Label(self.win,text=" ")
-		space.grid(row=2)
+		k = tk.Button(self.win, borderwidth=0, highlightthickness=0, command=self.atualiza_info)
+		self.img_salvar = ImageTk.PhotoImage(file="images/button_salvar.png")
+		k.config(image=self.img_salvar)
+		self.image = self.img_salvar
+		k.place(relx=0.3, rely=0.96, anchor="s")
 
-		k = ttk.Button(self.win, text="Salvar", command=self.atualiza_info)
-		k.grid(row=3, column=1)
-
-		d = ttk.Button(self.win, text="Apagar Nó", command=self.deleta_No)
-		d.grid(row=3,column= 2)
+		d = tk.Button(self.win, borderwidth=0, highlightthickness=0, command=self.deleta_No)
+		self.img_apagarNo = ImageTk.PhotoImage(file="images/button_apagar-no.png")
+		d.config(image=self.img_apagarNo)
+		self.image = self.img_salvar
+		d.place(relx=0.7, rely=0.96, anchor="s")
 
 		self.move_flag = False
 
 	def deleta_No(self):
 		self.canvas.delete(self.image_obj)
-		self.win.destroy()
 		self.canvas.delete(self.canvas_id)
+		self.__del__()
+		self.on = False
+		self.win.destroy()	
 
 	def atualiza_info(self):
 		self.nome_No = self.nome_NoAt.get()
@@ -139,8 +159,7 @@ class No(object):
 		if self.move_flag and self.qtdMoves==0:
 			new_xpos, new_ypos = event.x, event.y
 
-			self.canvas.move(self.image_obj,
-							 new_xpos - self.mouse_xpos, new_ypos - self.mouse_ypos)
+			self.canvas.move(self.image_obj, new_xpos - self.mouse_xpos, new_ypos - self.mouse_ypos)
 
 			self.mouse_xpos = new_xpos
 			self.mouse_ypos = new_ypos
@@ -202,18 +221,27 @@ class Rede(tk.Frame):
 		self.canvas.destroy()
 		self.deleta()
 		self.criar_Canvas()
-		self.canvas.configure(background= 'light blue')
+		self.canvas.configure(background= 'alice blue')
+		
+		self.criar_no = tk.Button(self.canvas, borderwidth=0, highlightthickness=0, command=self.criar_No) #, text="Nó", width="10",
+		self.img_criarNo = ImageTk.PhotoImage(file="images/button_adicionar-no.png")
+		self.criar_no.config(image=self.img_criarNo)
+		self.image = self.img_criarNo
+		self.criar_no.place(relx=.04, rely=0.96, anchor="w")
 
-		self.criar_no = tk.Button(self.canvas, text="Nó", width="10", command=self.criar_No)
-		self.criar_no.place(relx=.2, rely=0.96, anchor="w")
+		self.criar_aresta = tk.Button(self.canvas, borderwidth=0, highlightthickness=0, command=self.criar_Aresta)
+		self.img_criarAresta = ImageTk.PhotoImage(file = "images/button_adicionar-aresta.png")
+		self.criar_aresta.config(image=self.img_criarAresta)
+		self.image = self.img_criarAresta
+		self.criar_aresta.place(relx=.2, rely=.96, anchor="w")
 
-		self.salvar_topologia = tk.Button(self.canvas, text="Salvar topologia", width="15", command=self.salvar_Topologia)
-		self.salvar_topologia.place(relx=.6, rely=.96, anchor="e")
+		self.salvar_topologia = tk.Button(self.canvas, borderwidth=0, highlightthickness=0, command=self.salvar_Topologia)
+		self.img_topologia = ImageTk.PhotoImage(file="images/button_salvar-topologia.png")
+		self.salvar_topologia.config(image=self.img_topologia)
+		self.image = self.img_topologia
+		self.salvar_topologia.place(relx=0.96, rely=.96, anchor="e")
 
-		self.criar_aresta = tk.Button(self.canvas, text="Aresta", width="10", command=self.criar_Aresta)
-		self.criar_aresta.place(relx=.8, rely=.96, anchor="e")
-
-	def carregar_Topologia(self):
+	def carregar_Topologia(self): 
 		from tkinter import filedialog as fd
 		self.caminho = fd.askopenfilename()
 
@@ -255,7 +283,7 @@ class Rede(tk.Frame):
 		for i in range(len(self.grafo)):
 			for j in range(len(self.grafo)):
 				if (self.grafo[i][j]!=0 and i<=j):
-					novaAresta = Aresta(self.canvas,self.grafo[i][j], self.listadeNos, i, j)
+					novaAresta = Aresta(self.canvas,self.grafo[i][j], self.listadeNos, i, j, 10) #falta por o lambda
 
 					self.listadeNos[i].arestasAdj.append(novaAresta)
 					self.listadeNos[j].arestasAdj.append(novaAresta)
@@ -269,24 +297,42 @@ class Rede(tk.Frame):
 		self.win.geometry("%dx%d%+d%+d" % (230, 70, 100, 200))
 
 		l = tk.Label(self.win, text=text)
-		l.place(relx=0.5, rely=0.36, anchor="c")
+		l.place(relx=0.5, rely=0.3, anchor="c")
 
-		k = ttk.Button(self.win, text="OK", command= lambda: self.win.destroy())
-		k.place(relx=.5, rely=.8, anchor="c")
+		k = tk.Button(self.win, borderwidth=0, highlightthickness=0, command= lambda: self.win.destroy())
+		self.img_ok = ImageTk.PhotoImage(file="images/button_ok.png")
+		k.config(image=self.img_ok)
+		self.image = self.img_ok
+		k.place(relx=.5, rely=.76, anchor="c")
 
 	def salvar_Topologia(self):
+		for i in range(len(self.listadeNos)):
+			if self.listadeNos[i].on == False:
+				self.listadeNos.remove(self.listadeNos[i])
+			for j in range(len(self.listadeNos[i].arestasAdj)):
+				if(self.listadeNos[i].arestasAdj[j].on == False):
+					self.listadeNos[i].arestasAdj.remove(self.listadeNos[i].arestasAdj[j])
+
+		for i in range(len(self.listadeArestas)):
+			if self.listadeArestas[i].on == False:
+				self.listadeArestas.remove(self.listadeArestas[i])
+
 		self.win = tk.Toplevel()
 		self.win.wm_title("Salvar Topologia")
-		self.win.geometry("%dx%d%+d%+d" % (350, 50, 100, 200))
+		self.win.iconbitmap(r'images\favicon.ico')
+		self.win.geometry("%dx%d%+d%+d" % (300, 80, 100, 200))
 
 		l = tk.Label(self.win, text="Nome da Topologia: ")
-		l.grid(row=1,column=1)
+		l.place(relx=0.2, rely=0.3, anchor="c")
 
-		self.nomeTopologia = tk.Entry(self.win)
-		self.nomeTopologia.grid(row=1, column=2)
+		self.nomeTopologia = tk.Entry(self.win, width=20)
+		self.nomeTopologia.place(relx=0.6, rely=0.3, anchor="c")
 
-		k = ttk.Button(self.win, text="Salvar", command= self.criar_Dataframe)
-		k.grid(row=1, column=3)
+		k = tk.Button(self.win, borderwidth=0, highlightthickness=0, command= self.criar_Dataframe)
+		self.img_salvar = ImageTk.PhotoImage(file="images/button_salvar.png")
+		k.config(image=self.img_salvar)
+		self.image = self.img_salvar
+		k.place(relx=0.5, rely=0.8, anchor="c")
 
 	def criar_Dataframe(self):
 		arquivo = pd.DataFrame()
@@ -336,29 +382,32 @@ class Rede(tk.Frame):
 
 	def criar_Aresta(self):
 		self.win = tk.Toplevel()
-		self.win.wm_title("Adiciona Aresta")
-		self.win.geometry("%dx%d%+d%+d" % (350, 100, 100, 200))
-		l = tk.Label(self.win, text="Valor da Aresta: ")
-		l.grid(row=1, column=1)
-	
-		self.valorComprimento = tk.Entry(self.win)
-		self.valorComprimento.grid(row=1, column=2)
+		self.win.wm_title("Nova Aresta")
+		self.win.iconbitmap(r'images\favicon.ico')
+		self.win.geometry("%dx%d%+d%+d" % (250, 190, 100, 200))
+
 
 		lj = tk.Label(self.win, text="De:")
-		lj.grid(row=2, column=0)
+		lj.place(relx=0.4, rely=0.1, anchor="e")
 
 		
 		self.lista1 = tk.StringVar(self.win)
 		self.lista1.set(" ")
-		self.listaNomesVertices = [str(self.listadeNos[i].nome_No) for i in range(len(self.listadeNos))]
+		
+		self.listaNomesVertices = []
+
+		for i in range (len(self.listadeNos)):
+			if self.listadeNos[i].on == True:
+				self.listaNomesVertices.append(str(self.listadeNos[i].nome_No))
+
 		try:
 			w = tk.OptionMenu(self.win, self.lista1, *self.listaNomesVertices)
 		except:
 			pass
-		w.grid(row=2, column=1)
+		w.place(relx=0.6, rely=0.05, anchor="n")
 
 		iy = tk.Label(self.win, text="Para:")
-		iy.grid(row=2, column=2)
+		iy.place(relx=0.4, rely=0.3, anchor="e")
 
 
 		self.lista2 = tk.StringVar(self.win)
@@ -367,10 +416,27 @@ class Rede(tk.Frame):
 			r = tk.OptionMenu(self.win, self.lista2, *self.listaNomesVertices)
 		except:
 			pass
-		r.grid(row=2, column=3)
+		r.place(relx=0.6, rely=0.3, anchor="c")
 
-		k = ttk.Button(self.win, text="Adicionar Aresta", command= self.captura)
-		k.grid(row=3, column=2)
+		l = tk.Label(self.win, text="Comprimento: ")
+		l.place(relx=0.4, rely=0.5, anchor="e")
+	
+		self.valorComprimento = tk.Entry(self.win)
+		self.valorComprimento.place(relx=0.6, rely=0.5, width = 50, anchor="c")
+
+		l = tk.Label(self.win, text="Lambda (λ): ")
+		l.place(relx=0.4, rely=0.65, anchor="e")
+	
+
+		self.valorLambda = tk.Entry(self.win)
+		self.valorLambda.insert(0,"10")
+		self.valorLambda.place(relx=0.6, rely=0.65, width = 50, anchor="c")
+
+		k = tk.Button(self.win, borderwidth=0, highlightthickness=0, command= self.captura)
+		self.img_salvar = ImageTk.PhotoImage(file="images/button_salvar.png")
+		k.config(image=self.img_salvar)
+		self.image = self.img_salvar
+		k.place(relx=0.5, rely=0.98, anchor="s")
 
 
 	def captura(self):
@@ -382,7 +448,7 @@ class Rede(tk.Frame):
 			if (self.listadeNos[i].nome_No == self.lista2.get()):
 				item2 = i
 
-		novaAresta = Aresta(self.canvas, self.valorComprimento.get(), self.listadeNos, item1, item2)
+		novaAresta = Aresta(self.canvas, self.valorComprimento.get(), self.listadeNos, item1, item2, int(self.valorLambda.get()))
 		self.listadeNos[item1].arestasAdj.append(novaAresta)
 		self.listadeNos[item2].arestasAdj.append(novaAresta)
 
@@ -400,6 +466,8 @@ class Rede(tk.Frame):
 		import random
 		self.image_1 = No(self.canvas, "pc.png", random.randint(20,100), random.randint(20,100), cont)
 		self.listadeNos.append(self.image_1)
+		print("--------------------------")
+		print(self.listadeNos)
 
 def main():
 	app_win = tk.Tk()
@@ -407,9 +475,8 @@ def main():
 	app_win.geometry("+{}+{}".format(APP_XPOS, APP_YPOS))
 	app_win.geometry("{}x{}".format(APP_WIDTH, APP_HEIGHT))
 	app_win.resizable(width=False, height=False)
-	app_win.iconbitmap(r'C:\Users\luizb\Documents\Tópicos II\atividade9.1\images\lg.ico')
+	app_win.iconbitmap(r'images\favicon.ico')
 	app = Rede(app_win)
-	#.pack(fill='both', expand=True)
 	app_win.mainloop()
 
 
